d34f4a401b9f9d06ff248eba3b26e1fbd6cfb235
Victor Lahr <lahr.victor@gmail.com>
2023-04-25 17:21:07 -0300

    feat: implement retrieve user transactions

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,60 +1,101 @@
 // PACKAGE IMPORTS
-import React from 'react';
-import styled from 'styled-components';
-import { BiExit } from 'react-icons/bi';
+import axios from 'axios';
+import dayjs from 'dayjs';
+import React, { useEffect } from 'react';
 import { AiOutlineMinusCircle, AiOutlinePlusCircle } from 'react-icons/ai';
+import { BiExit } from 'react-icons/bi';
+import { Link, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 // VALUE EXPORTS
 export default function HomePage() {
+  const [transactions, setTransactions] = React.useState([]);
+  const navigate = useNavigate();
+  const { REACT_APP_API_URL } = process.env;
+  const token = localStorage.getItem('token');
+  function validateAccess() {
+    if (!token) {
+      // eslint-disable-next-line no-alert
+      alert('Please sign in to access the application!');
+      navigate('/');
+    }
+  }
+
+  const greetingMessage = `Olá, ${localStorage.getItem('user')}`;
+  const totalAmmount = 0;
+
+  function loadTransactions() {
+    axios.get(`${REACT_APP_API_URL}/transactions`, { headers: { Authorization: `Bearer ${token}` } })
+      .then((response) => {
+        setTransactions(response.data);
+      })
+
+      .catch((error) => {
+        // eslint-disable-next-line no-alert
+        alert(error.response.data);
+      });
+  }
+
+  useEffect(() => {
+    validateAccess();
+    loadTransactions();
+  }, []);
+
   return (
     <HomeContainer>
       <Header>
-        <h1>Olá, Fulano</h1>
-        <BiExit />
+        <h1>
+          {greetingMessage}
+        </h1>
+        <Link to="/">
+          <BiExit onClick={() => { localStorage.clear(); }} />
+        </Link>
       </Header>
 
       <TransactionsContainer>
         <ul>
-          <ListItemContainer>
-            <div>
-              <span>30/11</span>
-              <strong>Almoço mãe</strong>
-            </div>
-            <Value color={'negativo'}>120,00</Value>
-          </ListItemContainer>
-
-          <ListItemContainer>
-            <div>
-              <span>15/11</span>
-              <strong>Salário</strong>
-            </div>
-            <Value color={'positivo'}>3000,00</Value>
-          </ListItemContainer>
+          {transactions.map((transaction) => (
+            <ListItemContainer>
+              <div>
+                <span>{dayjs(transaction.date).format('MM/DD')}</span>
+                <strong>{transaction.description}</strong>
+              </div>
+              <Value color={transaction.type}>
+                {(transaction.value / 100).toLocaleString('pt-BR', { minimumFractionDigits: 2 })}
+              </Value>
+            </ListItemContainer>
+          ))}
         </ul>
 
         <article>
           <strong>Saldo</strong>
-          <Value color={'positivo'}>2880,00</Value>
+          <Value color={totalAmmount < 0 ? 'outflow' : 'inflow'}>
+            {(totalAmmount / 100).toLocaleString('pt-BR', { minimumFractionDigits: 2 })}
+          </Value>
         </article>
       </TransactionsContainer>
 
       <ButtonsContainer>
-        <button type="button">
-          <AiOutlinePlusCircle />
-          <p>
-            Nova
-            <br />
-            entrada
-          </p>
-        </button>
-        <button type="button">
-          <AiOutlineMinusCircle />
-          <p>
-            Nova
-            <br />
-            saída
-          </p>
-        </button>
+        <Link to="/nova-transacao/entrada">
+          <button type="button">
+            <AiOutlinePlusCircle />
+            <p>
+              Nova
+              <br />
+              entrada
+            </p>
+          </button>
+        </Link>
+        <Link to="/nova-transacao/saida">
+          <button type="button">
+            <AiOutlineMinusCircle />
+            <p>
+              Nova
+              <br />
+              saída
+            </p>
+          </button>
+        </Link>
       </ButtonsContainer>
 
     </HomeContainer>
@@ -66,6 +107,7 @@ const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
   height: calc(100vh - 50px);
+  width: 100%;
 `;
 const Header = styled.header`
   display: flex;
@@ -85,9 +127,10 @@ const TransactionsContainer = styled.article`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  overflow: scroll;
   article {
     display: flex;
-    justify-content: space-between;   
+    justify-content: space-between; 
     strong {
       font-weight: 700;
       text-transform: uppercase;
@@ -98,10 +141,10 @@ const ButtonsContainer = styled.section`
   margin-top: 15px;
   margin-bottom: 0;
   display: flex;
-  gap: 15px;
-  
+  justify-content: space-between;
+  width: 100%;
   button {
-    width: 50%;
+    width: 150px;
     height: 115px;
     font-size: 22px;
     text-align: left;
@@ -116,7 +159,7 @@ const ButtonsContainer = styled.section`
 const Value = styled.div`
   font-size: 16px;
   text-align: right;
-  color: ${(props) => (props.color === 'positivo' ? 'green' : 'red')};
+  color: ${(props) => (props.color === 'inflow' ? 'green' : 'red')};
 `;
 const ListItemContainer = styled.li`
   display: flex;
