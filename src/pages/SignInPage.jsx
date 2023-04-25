0c482de6aa460d10d6987efca232b4551fccd484
Victor Lahr <lahr.victor@gmail.com>
2023-04-25 07:57:47 -0300

    feat: implement user sign in

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,31 +1,81 @@
 // PACKAGE IMPORTS
+import axios from 'axios';
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 
 // VALUE IMPORTS
 import MyWalletLogo from '../components/MyWalletLogo';
 
 // VALUE EXPORTS
 export default function SignInPage() {
+  const [form, setForm] = React.useState({ email: '', password: '' });
+  const [isLoading, setIsLoading] = React.useState(false);
+  const navigate = useNavigate();
+  const { REACT_APP_API_URL } = process.env;
+
+  function userSignIn(event) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    axios.post(`${REACT_APP_API_URL}/sign-in`, form)
+      .then((response) => {
+        localStorage.setItem('bearer token', (response.data));
+        navigate('/home');
+      })
+
+      .catch((error) => {
+        // eslint-disable-next-line no-alert
+        alert(error.response.data);
+        setIsLoading(false);
+      });
+  }
+
+  function handleForm(event) {
+    const { name, value } = event.target;
+    setForm({ ...form, [name]: value });
+  }
+
   return (
-    <SingInContainer>
-      <form>
-        <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autoComplete="new-password" />
-        <button type="button">Entrar</button>
+    <SignInContainer>
+      <MyWalletLogo />
+
+      <form onSubmit={userSignIn}>
+        <input
+          name="email"
+          type="email"
+          placeholder="E-mail"
+          value={form.email}
+          onChange={handleForm}
+          disabled={isLoading}
+          required
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Senha"
+          value={form.password}
+          onChange={handleForm}
+          disabled={isLoading}
+          required
+        />
+        <button
+          type="submit"
+          disabled={isLoading}
+        >
+          Entrar
+        </button>
       </form>
 
-      <Link>
+      <Link to="/cadastro">
         Primeira vez? Cadastre-se!
       </Link>
-    </SingInContainer>
+    </SignInContainer>
   );
 }
 
 // STYLED COMPONENTS
-const SingInContainer = styled.section`
+const SignInContainer = styled.section`
   height: 100vh;
   display: flex;
   flex-direction: column;
