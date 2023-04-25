abb271fdc6b91fe6e876d1e88751a6cd7c4544b0
Victor Lahr <lahr.victor@gmail.com>
2023-04-25 11:53:33 -0300

    feat: implement user sign up

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,33 +1,114 @@
 // PACKAGE IMPORTS
+import axios from 'axios';
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 
 // VALUE IMPORTS
 import MyWalletLogo from '../components/MyWalletLogo';
 
 // VALUE EXPORTS
 export default function SignUpPage() {
+  const [form, setForm] = React.useState({
+    name: '',
+    email: '',
+    password: '',
+    passwordCheck: '',
+  });
+  const [isLoading, setIsLoading] = React.useState(false);
+  const navigate = useNavigate();
+  const { REACT_APP_API_URL } = process.env;
+
+  function userSignUp(event) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    if (form.password !== form.passwordCheck) {
+      // eslint-disable-next-line no-alert
+      alert('The passwords do not match!');
+      setIsLoading(false);
+      return;
+    }
+
+    axios.post(`${REACT_APP_API_URL}/sign-up`, {
+      name: form.name,
+      email: form.email,
+      password: form.password,
+    })
+      .then(() => {
+        navigate('/');
+      })
+
+      .catch((error) => {
+        // eslint-disable-next-line no-alert
+        alert(error.response.data);
+        setIsLoading(false);
+      });
+  }
+
+  function handleForm(event) {
+    const { name, value } = event.target;
+    setForm({ ...form, [name]: value });
+  }
+
   return (
-    <SingUpContainer>
-      <form>
-        <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autoComplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autoComplete="new-password" />
-        <button type="button">Cadastrar</button>
+    <SignUpContainer>
+      <MyWalletLogo />
+
+      <form onSubmit={userSignUp}>
+        <input
+          name="name"
+          type="text"
+          placeholder="Nome"
+          value={form.name}
+          onChange={handleForm}
+          disabled={isLoading}
+          required
+        />
+        <input
+          name="email"
+          type="email"
+          placeholder="E-mail"
+          value={form.email}
+          onChange={handleForm}
+          disabled={isLoading}
+          required
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Senha"
+          value={form.password}
+          onChange={handleForm}
+          disabled={isLoading}
+          required
+        />
+        <input
+          name="passwordCheck"
+          type="password"
+          placeholder="Confirme a senha"
+          value={form.passwordCheck}
+          onChange={handleForm}
+          disabled={isLoading}
+          required
+        />
+        <button
+          type="submit"
+          disabled={isLoading}
+        >
+          Cadastrar
+        </button>
       </form>
 
-      <Link>
+      <Link to="/">
         Já tem uma conta? Entre agora!
       </Link>
-    </SingUpContainer>
+    </SignUpContainer>
   );
 }
 
 // STYLED COMPONENTS
-const SingUpContainer = styled.section`
+const SignUpContainer = styled.section`
   height: 100vh;
   display: flex;
   flex-direction: column;
